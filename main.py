79562b007ee493a66ebceb71e03de6fad074df50
Yong Hoon Do <ydo@twitter.com>
2021-12-28 22:01:34 -0800

    refines the search query for better accuracy

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import json
 from ytmusicapi import YTMusic
 from os import listdir
 from os.path import isfile, join
+import glob
+import os
+import regex
 
 class Loader:
     def __init__(self):
@@ -40,16 +43,26 @@ class Parser:
                 res.append({ "title": a.find("a").text, "artist": b.find("a").text, "album": c.text })
         return (playlisttitle, res)
 
-class OutputWriter:
+class Builder:
     def __init__(self):
         pass
 
-    def generate_file(self, ptitle, body):
-        with open('playlists/' + ptitle + '.json', 'w') as outfile:
-            pretty_body = json.dumps(body, indent=2)
-            outfile.write(pretty_body)
-    
-    def build_search_queries(self):
+    def __delete_hangul(self, s):
+        is_hangul = lambda x: True if regex.search(r'\p{IsHangul}', x) else False
+        i = s.find('(')
+        j = s.find(')')
+        candidate = s[i+1:j]
+        if is_hangul(candidate):
+            res = s[:i] + s[j+1:]
+            return res
+        return s
+
+    def build_search_query(self, playlist_entity):
+        title, song_entities = playlist_entity
+        queries = [" ".join([self.__delete_hangul(song['artist']), song['title'], song['album']]) for song in song_entities]
+        return (title, queries)
+
+    def build_search_queries_from_files(self):
         res = list()
         playlists = [f for f in listdir('playlists') if isfile(join('playlists', f))]
         for filename in playlists:
@@ -59,38 +72,75 @@ class OutputWriter:
                 res.append((filename.replace(".json", ""), queries))
         return res
 
+
+    def generate_file(self, ptitle, body):
+        with open('playlists/' + ptitle + '.json', 'w') as outfile:
+            pretty_body = json.dumps(body, indent=2)
+            outfile.write(pretty_body)
+
+    def cleanup_json_files(self):
+        files = glob.glob('playlists/*.json')
+        for f in files:
+            try:
+                os.remove(f)
+                print('✅ file deleted: "{}"'.format(f))
+            except OSError as e:
+                print("Error: {} : {}".format(f, e.strerror))
+
+class Logger:
+    def __init__(self):
+        self.notfounds = list()
+
+    def add_song_not_found(self, search_entity):
+        self.notfounds.append(search_entity)
+
+    def write_not_founds(self):
+        if len(self.notfounds) > 0:
+            for entity in self.notfounds:
+                title, body = entity
+                with open('not_founds.txt', 'a') as outfile:
+                    line = "[{}]: {}".format(title, body)
+                    outfile.write(line)
+                    outfile.write("\n")
+
 '''
 1. read file from albums.txt -> [link]
 2. [link] -> [html_dom]
 3. [html_dom] -> (title, [song_entity])
-4. (title, [song_entity]) -> playlists/title.json
-5. title.json -> ytmusic playlist
+4. (title, [song_entity]) -> (title, [search_entity])
+5. (title, [search_entity]) -> ytmusic playlist
 '''
 
 loader = Loader()
 parser = Parser()
+builder = Builder()
+logger = Logger()
 album_links = loader.load_playlist_links('albums.txt')
 album_html_doms = [Loader().load_html(album_link) for album_link in album_links]
-song_entities = [parser.generate_json(album_html_dom) for album_html_dom in album_html_doms]
-for song_title, song_metadata_body in song_entities:
-    OutputWriter().generate_file(song_title, song_metadata_body)
-    print('wrote a file (\"{}.json\") w/ {} of songs'.format(song_title, len(song_metadata_body)))
+playlist_entities = [parser.generate_json(album_html_dom) for album_html_dom in album_html_doms]
+query_entities = [builder.build_search_query(entity) for entity in playlist_entities]
 
-queries = OutputWriter().build_search_queries()
 ytmusic = YTMusic('headers_auth.json')
-for query in queries:
+for query in query_entities:
     title, search_queries = query
     print(title)
     playlistId = ytmusic.create_playlist(title, title)
     for q in search_queries:
         search_results = ytmusic.search(q)
         if len(search_results) == 0:
-            print('❌ "{}"'.format(q))
+            print('- ❌ "{}"'.format(q))
+            logger.add_song_not_found((title, q))
             continue
 
         search_result = search_results[0]
         if 'videoId' in search_result:
             ytmusic.add_playlist_items(playlistId, [search_result['videoId']])
-            print('✅ "{}"'.format(q))
+            print('- ✅ "{}"'.format(q))
         else:
-            print('❌ "{}"'.format(q))
+            print('- ❌ "{}"'.format(q))
+            logger.add_song_not_found((title, q))
+    print("\n")
+
+builder.cleanup_json_files()
+logger.write_not_founds()
+print("Done!\n")
